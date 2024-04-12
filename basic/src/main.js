efc7bb89e0264abba0325fdd4be7b14ed71e863a
shin <sinej1020@gmail.com>
2024-04-12 11:15:14 +0900

    animation

--- basic/src/main.js
+++ basic/src/main.js
@@ -5,10 +5,7 @@
 // import example from "./ex05";
 // import example from "./ex06";
 // import example from "./ex06_02";
-import example from "./ex07";
-// import example from "./ex08";
-// import example from "./ex09";
-// import example from "./ex10";
-// import example from "./ex11";
+// import example from "./ex07";
+import example from "./ex08";
 
 example();
