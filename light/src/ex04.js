6a8486439b58e9c89f8580d283d08b8603b0fe15
shin <sinej1020@gmail.com>
2024-04-15 19:27:32 +0900

    light 추가

---
+++ light/src/ex04.js
@@ -0,0 +1,106 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { RectAreaLightHelper } from 'three/examples/jsm/helpers/RectAreaLightHelper';
+import dat from 'dat.gui';
+
+// ----- 주제: 
+
+export default function example() {
+	// Renderer
+	const canvas = document.querySelector('#three-canvas');
+	const renderer = new THREE.WebGLRenderer({
+		canvas,
+		antialias: true
+	});
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1);
+	renderer.shadowMap.enabled = true;
+
+	// Scene
+	const scene = new THREE.Scene();
+
+	// Camera
+	const camera = new THREE.PerspectiveCamera(
+		75,
+		window.innerWidth / window.innerHeight,
+		0.1,
+		1000
+	);
+	camera.position.y = 1.5;
+	camera.position.z = 4;
+	scene.add(camera);
+
+	// Light
+	const ambientLight = new THREE.AmbientLight("white", .5);
+	scene.add(ambientLight);
+
+	const light = new THREE.RectAreaLight('orange', 10, 2, 2);
+	light.position.y = 2;
+	light.position.z = 4;
+	scene.add(light);
+
+	const lightHelper = new RectAreaLightHelper(light);
+	scene.add(lightHelper);
+
+	const gui = new dat.GUI();
+
+	// Controls
+	const controls = new OrbitControls(camera, renderer.domElement);
+
+	// Geometry
+	const planeGeometry = new THREE.PlaneGeometry(10, 10);
+	const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
+	const sphereGeometry = new THREE.SphereGeometry(.7, 16, 16);
+
+	// material
+	const material1 = new THREE.MeshStandardMaterial({color: 'white'});
+	const material2 = new THREE.MeshStandardMaterial({color: 'white'});
+	const material3 = new THREE.MeshStandardMaterial({color: 'white'});
+
+	// Mesh
+	const plane = new THREE.Mesh(planeGeometry, material1);
+	const box = new THREE.Mesh(boxGeometry, material2);
+	const sphere = new THREE.Mesh(sphereGeometry, material3);
+	scene.add(plane, box, sphere);
+
+	plane.rotation.x = -Math.PI * .5;
+	box.position.set(1,1,0);
+	sphere.position.set(-1,1,0);
+
+	plane.receiveShadow = true;
+	box.castShadow = true;
+	box.receiveShadow = true;
+	sphere.castShadow = true;
+	sphere.receiveShadow = true
+
+
+	// AxesHelper
+	const axesHelper = new THREE.AxesHelper(3);
+	scene.add(axesHelper);
+
+	// Dat GUI
+	gui.add(light.position, 'x', -5, 5, 0.1).name('카메라 X');
+	gui.add(light.position, 'y', -5, 5, 0.1).name('카메라 Y');
+	gui.add(light.position, 'z', 2, 10, 0.1).name('카메라 Z');
+
+	// 그리기
+	const clock = new THREE.Clock();
+
+	function draw() {
+
+		renderer.render(scene, camera);
+		renderer.setAnimationLoop(draw);
+	}
+
+	function setSize() {
+		camera.aspect = window.innerWidth / window.innerHeight;
+		camera.updateProjectionMatrix();
+		renderer.setSize(window.innerWidth, window.innerHeight);
+		renderer.render(scene, camera);
+	}
+
+	// 이벤트
+	window.addEventListener('resize', setSize);
+
+	draw();
+}
